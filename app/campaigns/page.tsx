9d2501c72507a0514f81d8ab97f41cc5e8105c2a
pandupan <pangestup60@gmail.com>
2023-09-10 13:33:40 +0700

    [ADD] - Campagains Carousel Section

---
+++ app/campaigns/page.tsx
@@ -0,0 +1,12 @@
+import CampagainCarousel from '@/components/pages/Campaigns/CampagainCarousel'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <CampagainCarousel/>
+    </div>
+  )
+}
+
+export default page
