f3b1db60fe964ae51c97149d360432a471d4ab60
EgaAprianto <egaaprianto04@gmail.com>
2023-09-10 12:04:45 +0700

    [ADD - HowItWorks Page]

---
+++ app/works/page.tsx
@@ -0,0 +1,12 @@
+import EmpoyerCom from '@/components/pages/HowItWorks/EmpoyerCom'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <EmpoyerCom/>
+    </div>
+  )
+}
+
+export default page
