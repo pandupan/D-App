14598e30765acd98483748635f67aa3011302b38
EgaAprianto <egaaprianto04@gmail.com>
2023-09-10 12:27:00 +0700

    [ADD] - Choose a Zip Code at Works

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -1,10 +1,12 @@
 import EmpoyerCom from '@/components/pages/HowItWorks/EmpoyerCom'
+import SignUpVer from '@/components/pages/HowItWorks/SignUpVer'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
       <EmpoyerCom/>
+      <SignUpVer/>
     </div>
   )
 }
