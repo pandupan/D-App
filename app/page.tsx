61eb36a614767dbf53f4d467e901c7db990ecdb6
EgaAprianto <egaaprianto04@gmail.com>
2023-09-10 09:46:34 +0700

    [FIXED] - MergeConflict

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import EarnUnique from '@/components/pages/Home/EarnUnique'
 import HeroSection from '@/components/pages/Home/HeroSection'
 import ImagePlaceholder from '@/components/pages/Home/ImagePlaceholder'
 import JoinDonateReceive from '@/components/pages/Home/JoinDonateReceive'
+import MicroCrowdfunding from '@/components/pages/Home/MicroCrowdfunding'
 import SupportLocal from '@/components/pages/Home/SupportLocal'
 
 export default function Home() {
@@ -9,7 +10,7 @@ export default function Home() {
     <>
       <HeroSection/>
       <ImagePlaceholder/>
-      <SupportLocal/>
+      <MicroCrowdfunding/>
       <EarnUnique/>
       <JoinDonateReceive/>
       <SupportLocal/>
--- components/pages/Home/SupportLocal.tsx
+++ components/pages/Home/SupportLocal.tsx
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import Image from "next/image";
-import {BiSolidCube} from 'react-icons/bi'
-import ButtonOutline from "@/components/core/buttons/ButtonOutline";
-
-const SupportLocal = () => {
-  return(
-    <div className="flex justify-between container my-48">
-      <div className="flex flex-col gap-10 w-[600px] mt-32">
-        <BiSolidCube size = {70}/>
-        <h1 className="font-black text-5xl ">
-          Support Local Causes with Micro-Crowdfunding
-        </h1>
-        <p>
-        Donate to micro-crowdfunding campaigns in your local zip code and make a difference in causes like clean water, clothing, and food.
-        </p>
-        <div className="flex flex-row gap-3">
-            <ButtonOutline title = {"Donate"}/>        
-            <div className="flex my-auto">
-              Learn More
-            </div>
-            <div className="flex mt-[10px]">
-            <BiSolidCube size = {22}/>
-            </div>
-        </div>
-      </div>
-      <div className=" relative w-[650px] h-[650px]">
-        <Image 
-          src="/images/placeHolder_2.png"
-          alt="Placeholder"
-          fill={true}
-        />
-=======
 import ButtonOutline from "@/components/core/buttons/ButtonOutline"
 import ButtonPrimary from "@/components/core/buttons/ButtonPrimary"
 
@@ -47,14 +13,9 @@ const SupportLocal = () => {
         <ButtonPrimary title="Sign Up"/>
         <ButtonOutline title="Learn More"/>
 
->>>>>>> cc6a39bab3d0b51a0e843625af17e1867dd08cb5
       </div>
     </div>
   )
 }
 
-<<<<<<< HEAD
 export default SupportLocal
-=======
-export default SupportLocal
->>>>>>> cc6a39bab3d0b51a0e843625af17e1867dd08cb5
