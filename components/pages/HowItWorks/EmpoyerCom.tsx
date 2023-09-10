0d0478b8bb6c230555a4955cc9d7252506e62313
EgaAprianto <egaaprianto04@gmail.com>
2023-09-10 12:10:42 +0700

    [ADD] - EmpoyerCom

--- components/pages/HowItWorks/EmpoyerCom.tsx
+++ components/pages/HowItWorks/EmpoyerCom.tsx
@@ -2,26 +2,21 @@ import React from "react";
 import Image from "next/image";
 import {BiSolidCube} from 'react-icons/bi'
 import ButtonOutline from "@/components/core/buttons/ButtonOutline";
+import ButtonPrimary from "@/components/core/buttons/ButtonPrimary";
 
 const EmpoyerCom = () => {
   return(
-    <div className="flex justify-between container my-48">
-      <div className="flex flex-col gap-10 w-[600px] mt-32">
-        <BiSolidCube size = {70}/>
-        <h1 className="font-black text-5xl ">
-        Revolutionizing Micro-Crowdfunding with Stablecoins and NFTs
+    <div className="flex justify-between container my-28">
+      <div className="flex flex-col gap-10 w-[616px] mt-32">
+        <h1 className="font-black text-6xl ">
+        Empower Your Community with F-N-F Dapp
         </h1>
         <p>
-        Welcome to the F-N-F Dapp, a decentralized micro-crowdfunding platform that aims to transform the way we give and receive through the use of stablecoins and NFTs.
+        Discover how the F-N-F Dapp revolutionizes micro-crowdfunding to create positive change in your local community.
         </p>
         <div className="flex flex-row gap-3">
-            <ButtonOutline title = {"Learn More"}/>        
-            <div className="flex my-auto">
-              Sign Up
-            </div>
-            <div className="flex mt-[10px]">
-            <BiSolidCube size = {22}/>
-            </div>
+          <ButtonPrimary title = {"Get Started"}/>  
+          <ButtonOutline title = {"Learn More"}/>  
         </div>
       </div>
       <div className=" relative w-[656px] h-[640px]">
