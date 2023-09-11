e61a464093fac72ef28105aeffeda300fefe3063
EgaAprianto <egaaprianto04@gmail.com>
2023-09-11 19:58:32 +0700

    [ADD Responsive] - Donate Page Done

--- components/pages/Donate/DonateLocal.tsx
+++ components/pages/Donate/DonateLocal.tsx
@@ -4,59 +4,60 @@ import {BiSolidCube} from "react-icons/bi"
 
 const DonateLocal = () => {
   return (
-    <div className="flex flex-col justify-center container items-center my-28">
+    <div className="flex flex-col justify-center sm:container sm:px-0 px-4  sm:mt-28 sm:mb-28 mt-14 mb-14">
       <div className="flex flex-col gap-6 text-center">
-        <div className="font-semibold">Empower</div>
-        <h1 className="text-4xl font-bold w-[650px]">
+        <div className="font-semibold sm:text-base text-sm">Empower</div>
+        <h1 className="sm:text-4xl text-3xl font-bold sm:w-[650px]">
           Donate to Local Crowdfunding Campaigns Today
         </h1>
-        <p className="w-[680px]">
+        <p className="sm:w-[680px] sm:text-base text-sm">
           Search for active campaigns in your zip code and choose to donate $1,
           $5, or $10 in USDC to make a difference in your community.
         </p>
       </div>
 
-      <div className="flex flex-row mt-[50px] gap-12">
-        <div className="flex flex-col w-[405px] gap-6">
-          <div className="relative w-[405px] h-[240px]">
+      <div className="flex sm:flex-row flex-col mt-[50px] gap-12">
+
+        <div className="flex flex-col sm:w-[405px] gap-6">
+          <div className="relative sm:w-[405px] sm:h-[240px] w-full h-[170px]">
             <Image src="/images/PlaceHolder_3.png" alt="Icon" fill={true} />
           </div>
-          <h1 className="px-2 text-center font-black text-3xl">
+          <h1 className="sm:px-2 text-center font-black sm:text-3xl text-xl">
             Find Local Campaigns in Your Zip Code
           </h1>
-          <p className="text-center">
+          <p className="text-center sm:text-base text-sm">
             Discover and support crowdfunding initiatives near you.
           </p>
         </div>
 
-        <div className="flex flex-col w-[405px] gap-6">
-          <div className="relative w-[405px] h-[240px]">
+        <div className="flex flex-col sm:w-[405px] gap-6">
+          <div className="relative sm:w-[405px] sm:h-[240px] w-full h-[170px]">
             <Image src="/images/PlaceHolder_3.png" alt="Icon" fill={true} />
           </div>
-          <h1 className="px-2 text-center font-black text-3xl">
+          <h1 className="sm:px-2 text-center font-black sm:text-3xl text-xl">
             Donate with Ease and Security
           </h1>
-          <p className="text-center">
+          <p className="text-center sm:text-base text-sm">
             Contribute to campaigns using MetaMask or your in-house ERC 1155
             F-N-F blockchain account.
           </p>
         </div>
 
-        <div className="flex flex-col w-[405px] gap-6">
-          <div className="relative w-[405px] h-[240px]">
+        <div className="flex flex-col sm:w-[405px] gap-6">
+          <div className="relative sm:w-[405px] sm:h-[240px] w-full h-[170px]">
             <Image src="/images/PlaceHolder_3.png" alt="Icon" fill={true} />
           </div>
-          <h1 className="px-2 text-center font-black text-3xl">
+          <h1 className="px-2 text-center font-black sm:text-3xl text-xl">
             Join Waitlists for Future Opportunities
           </h1>
-          <p className="text-center">
+          <p className="text-center sm:text-base text-sm">
             If a campaign is fully funded, join the waitlist for a chance to
             contribute later.
           </p>
         </div>
       </div>
 
-      <div className="flex flex-row justify-center items-center gap-4 mt-14">
+      <div className="flex flex-row justify-center items-center gap-4 sm:mt-14 mt-8">
         <ButtonOutline title={"Search"} />
         <div className="flex flex-row gap-2 place-self-center">
           Donate
--- components/pages/Donate/HeroDonate.tsx
+++ components/pages/Donate/HeroDonate.tsx
@@ -3,17 +3,17 @@ import Image from 'next/image'
 
 const HeroDonate = () => {
   return (
-    <div className="flex justify-start w-full h-[900px]">
-              <div className="relative w-full h-[900px]">
+    <div className="flex justify-start w-full sm:h-[900px] h-[812px]">
+              <div className="relative w-full sm:h-[900px] h-[812px]">
                 <Image
                   src="/images/PlaceHolder_3.png"
                   alt="Icon"
                   fill={true}
                 />
               </div>
-              <div className=' my-[300px] container absolute flex flex-col text-white gap-8'>
-                <h1 className='text-6xl font-black w-[560px]'>Donate to Local Zip Code Campaigns</h1>
-                <p className='w-[560px]'>Make a difference in your community by donating to micro-crowdfunded campaigns in your local zip code.</p>
+              <div className=' my-[300px] sm:container sm:px-0 px-4 absolute flex flex-col text-white gap-8'>
+                <h1 className='sm:text-6xl text-4xl font-black sm:w-[560px]'>Donate to Local Zip Code Campaigns</h1>
+                <p className='sm:w-[560px] sm:text-base text-sm'>Make a difference in your community by donating to micro-crowdfunded campaigns in your local zip code.</p>
                 <div className='flex flex-row gap-4'>
                   <button className='text-white bg-black flex justify-center place-items-center w-[99px] h-[48px]'>
                   Donate
