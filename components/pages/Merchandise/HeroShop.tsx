f5b3f3aafd5107a1d4fcc0274e0a25483d756108
EgaAprianto <egaaprianto04@gmail.com>
2023-09-11 20:53:19 +0700

    [ADD Responsive] - merch page DONE

--- components/pages/Merchandise/HeroShop.tsx
+++ components/pages/Merchandise/HeroShop.tsx
@@ -1,8 +1,8 @@
 const HeroShop = () => {
   return (
-    <div className="flex container justify-between my-28">
-      <h1 className="text-6xl font-black w-[320px]">Shop F-N-F Merchandise</h1>
-      <p className="w-[616px] h-[81px] text-lg">
+    <div className="flex sm:flex-row flex-col sm:container sm:px-0 px-4 sm:justify-between sm:mt-28 sm:mb-28 ">
+      <h1 className="sm:text-6xl text-4xl font-black w-[320px]">Shop F-N-F Merchandise</h1>
+      <p className="sm:w-[616px] sm:h-[81px] w-full h-[120px] sm:text-lg text-sm sm:mt-0 mt-6">
         Explore our exclusive collection of F-N-F merchandise and show your
         support with F-N-F stablecoin. From t-shirts and hats to coffee mugs and
         home decor, we have something for everyone.
--- components/pages/Merchandise/MessageMerch.tsx
+++ components/pages/Merchandise/MessageMerch.tsx
@@ -2,7 +2,7 @@ import { AiFillStar } from "react-icons/ai";
 import Image from "next/image";
 const MessageMerch = () => {
   return (
-    <div className="flex flex-col container justify-center my-28">
+    <div className="flex flex-col container justify-center sm:mt-28 sm:mb-28 mt-12 mb-28">
       <div className="flex flex-row justify-center">
         <AiFillStar size={22} />
         <AiFillStar size={22} />
@@ -11,7 +11,7 @@ const MessageMerch = () => {
         <AiFillStar size={22} />
       </div>
       <div className="flex justify-center">
-        <p className="w-[768px] text-center text-2xl font-bold mt-6">
+        <p className="sm:w-[768px] text-center sm:text-2xl text-lg font-bold mt-6">
           &quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           Suspendisse varius enim in eros elementum tristique. Duis cursus, mi
           quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero
@@ -19,17 +19,17 @@ const MessageMerch = () => {
         </p>
       </div>
 
-      <div className="flex flex-row place-self-center mt-[40px]">
-      <div className="flex flex-row w-[270px] justify-center items-center place-self-center gap-4 border-r-2 border-black">
-        <div className="relative w-[40px] h-[40px]">
+      <div className="flex flex-col sm:flex-row place-self-center text-center sm:text-left sm:mt-[40px] mt-[20px]">
+      <div className="flex flex-col sm:flex-row w-[270px] justify-center items-center place-self-center gap-4 sm:border-r-2 sm:border-black">
+        <div className="relative w-[56px] h-[56px]">
           <Image src="/images/Placeholder_Avatar.png" alt="Icon" fill={true} />
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col sm:my-0 mb-6">
           <h3 className="text-sm font-bold">Name Surname</h3>
-          <p>Position, Company name</p>
+          <p className="">Position, Company name</p>
         </div>
       </div>
-      <div className=" w-[114px] h-[28px]">
+      <div className=" w-[114px] h-[28px] flex justify-center place-self-center">
         <div className="relative  w-[114px] h-[28px] place-self-center my-2 mx-2">
         <Image 
           src="/images/WebflowIcon.png"
