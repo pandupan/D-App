fcfcd8956328856ca7e3f389244acd6046405fad
pandupan <pangestup60@gmail.com>
2023-09-13 15:06:48 +0700

    [ADD] - Some Page

--- components/pages/Merchandise/HeroMerch.tsx
+++ components/pages/Merchandise/HeroMerch.tsx
@@ -4,23 +4,23 @@ import Image from 'next/image'
 
 const HeroMerch = () => {
   return (
-    <div className="flex justify-start w-full sm:h-[497px] h-[335px]">
-              <div className="relative w-full sm:h-[497px] h-[335px]">
+    <div className="flex justify-start w-full sm:h-[421px] h-[354px]">
+              <div className="relative w-full sm:h-[421px] h-[354px]">
                 <Image
-                  src="/images/PlaceHolder_3.png"
+                  src="/images/placeHolder_3.png"
                   alt="Icon"
                   fill={true}
                 />
               </div>
-              <div className=' place-self-center sm:mx-10 sm:container px-4 absolute flex flex-col text-white sm:gap-8 gap-4'>
-                <h1 className='sm:text-5xl text-3xl font-black sm:w-[768px] sm:h-[58px] w-[250px] h-[86px]'>Medium length heading goes here</h1>
-                <p className='sm:w-[680px] sm:h-[54px] w-[270px] h-[72px] sm:text-base text-sm'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+              <div className=' my-[122px] sm:container sm:px-8 px-4 absolute flex flex-col place-self-center text-white gap-4'>
+                <h1 className='sm:text-5xl text-4xl font-black '>Medium length heading here</h1>
+                <p className='sm:text-base text-sm'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
                 <div className='flex flex-row gap-4'>
                   <button className='text-white bg-black flex justify-center place-items-center w-[103px] h-[48px]'>
-                  Button
+                    Sign Up
                   </button>
                   <button className='text-white border-white border flex justify-center place-items-center w-[103px] h-[48px]'>
-                  Button
+                    Donate
                   </button>
                 </div>
               </div>
