8fe708b6ced4a28c4faba9d00807f2714beb26a3
pandupan <pangestup60@gmail.com>
2023-09-13 16:15:25 +0700

    [FIX] - Team Page

--- components/pages/MeetTeam/OurTeam.tsx
+++ components/pages/MeetTeam/OurTeam.tsx
@@ -15,7 +15,7 @@ const OurTeam = () => {
         </p>
       </div>
       <div className="flex justify-start">
-        <div className="mt-8 grid grid-cols-1 grid-rows-1 sm:grid-cols-3 sm:grid-rows-2 gap-20">
+        <div className="mt-8 grid grid-cols-1 grid-rows-1 md:grid-cols-2 md:grid-rows-2 xl:grid-cols-3 xl:grid-rows-2 gap-20">
           <div>
             <div className=" relative aspect-square lg:w-[405px] lg:h-[395px]">
               <Image
