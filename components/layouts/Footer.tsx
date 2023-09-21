08cc03c641a1ed574769601eed56fc232983b5a8
EgaAprianto <egaaprianto04@gmail.com>
2023-09-21 16:16:24 +0700

    [FIX] FOOTER

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -1,46 +1,88 @@
-"use client";
+import React from "react";
+import { BiLogoFacebook } from "react-icons/bi";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
+import { BiLogoYoutube } from "react-icons/bi";
 
-import { usePathname } from "next/navigation";
-import Footer1 from "./footer/Footer1";
-import Footer2 from "./footer/Footer2";
-import Footer3 from "./footer/Footer3";
-import Footer4 from "./footer/Footer4";
-import Footer5 from "./footer/Footer5";
-import Footer6 from "./footer/Footer6";
-import Footer7 from "./footer/Footer7"
-
-const foot1 = ["/"]
-const foot2 = ["/teams"]
-const foot3 = ["/works"]
-const foot4 = ["/campaigns"]
-const foot5 = ["/donate"]
-const foot6 = ["/merch", "/product"]
-const foot7 = ["/details"]
-
-
-const Footer = () => {
-  const pathname = usePathname()
-
+const Footer1 = () => {
   return (
-    <div>
+    <div className="w-full">
+      <div className="sm:container px-4 sm:px-16">
+        <div className="font-bold text-3xl">
+          <h2 className="sm:text-3xl text-3xl">Logo</h2>
+        </div>
+        <div className="flex sm:flex-row flex-col sm:justify-between justify-start">
+          <div className="mt-8">
+            {/* <h3 className="font-medium text-sm">Address:</h3>
+            <p className="text-sm">Level 1, 12 Sample St, Sydney NSW 2000</p>
+            <h3 className="font-medium text-sm mt-6">Contact:</h3>
+            <p className="text-sm underline underline-offset-3">
+              1800 123 4567
+            </p>
+            <p className="text-sm underline underline-offset-3">
+              info@relume.io
+            </p>
 
-      {foot1.includes(pathname) && ( <Footer1/> )}
+            <div className="flex flex-row mt-6 gap-3 w-[168px]">
+              <BiLogoFacebook size={25} />
+              <AiOutlineInstagram size={25} />
+              <AiOutlineTwitter size={25} />
+              <AiFillLinkedin size={25} />
+              <BiLogoYoutube size={25} />
+            </div> */}
+          </div>
 
-      {foot2.includes(pathname) && ( <Footer2/> )}
-
-      {foot3.includes(pathname) && ( <Footer3/> )}
-
-      {foot4.includes(pathname) && ( <Footer4/> )} 
-
-      {foot5.includes(pathname) && ( <Footer5/> )}
-
-      {foot6.includes(pathname) && ( <Footer6/> )}
-
-      {foot7.includes(pathname) && ( <Footer7/> )}
-
-
+          <div className="flex sm:flex-row flex-col gap-8 mt-10 sm:mt-0 sm:justify-between sm:gap-x-20 font-semibold sm:text-base text-sm sm:mr-16">
+            {/* <div className="flex flex-col gap-y-3">
+              <h2 className="">About Us</h2>
+              <h2 className="">Services</h2>
+              <h2 className="">Products</h2>
+              <h2 className="">Blog</h2>
+              <h2 className="">Contact</h2>
+            </div>
+            <div className="flex flex-col gap-y-3">
+              <h2 className="">FAQ</h2>
+              <h2 className="">Support</h2>
+              <h2 className="">Terms</h2>
+              <h2 className="">Privacy</h2>
+              <h2 className="">Sitemap</h2>
+            </div> */}
+            {/* new */}
+            <div className="flex flex-col gap-y-3 items-center">
+              <h2 className="">Meet Team</h2>
+              <h2 className="">How it Works</h2>
+              <h2 className="">Donate</h2>
+              <h2 className="">Contact</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="border-t-[2px] border-black w-[90%] mx-auto sm:mt-20 mt-12" />
+      {/* old */}
+      {/* <div className="flex sm:container px-4 sm:px-16 sm:flex-row flex-col-reverse sm:justify-between gap-5  mt-8 sm:mb-28 mb-14">
+        <h3 className="text-sm">
+          © 2023 Example Company. All rights reserved.
+        </h3>
+        <div className="flex sm:flex-row flex-col mt-4 sm:mt-0 gap-4 text-sm underline underline-offset-3 sm:gap-10">
+          <h3>Privacy Policy</h3>
+          <h3>Terms of Service</h3>
+          <h3>Cookie Policy</h3>
+        </div>
+      </div> */}
+      {/* new */}
+      <div className="flex justify-center sm:container px-4 sm:px-16 sm:flex-row flex-col-reverse sm:justify-between gap-5  mt-8 sm:mb-28 mb-14">
+        <h3 className="text-sm text-center sm:text-start">
+          © 2023 Example Company. All rights reserved.
+        </h3>
+        <div className="text-center sm:text-start flex sm:flex-row flex-col mt-4 sm:mt-0 gap-4 text-sm underline underline-offset-3 sm:gap-10">
+          <h3>Privacy Policy</h3>
+          <h3>Terms of Service</h3>
+          <h3>Cookie Policy</h3>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer1;
